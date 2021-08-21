b7b82a8796223e495ce82a475c4839cb969be4d8
Yuya Matsuo <niu@niusounds.com>
2021-08-21 18:03:48 +0900

    use viewBinding

--- flowsample/src/main/java/com/niusounds/flowsample/MainActivity.kt
+++ flowsample/src/main/java/com/niusounds/flowsample/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.niusounds.flowsample.databinding.ActivityMainBinding
 import com.niusounds.libreastream.flow.AudioTrackOutput
 import com.niusounds.libreastream.flow.ReaStreamReceiver
 import kotlinx.coroutines.flow.collect
@@ -13,20 +14,21 @@ import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val text: TextView = findViewById(R.id.text)
-        lifecycleScope.launch {
-            val packets = ReaStreamReceiver().packets
-            launch {
-                AudioTrackOutput().play(packets)
-            }
-            launch {
-                packets.filter { it.isMidi }
-                    .collect { packet ->
-                        packet.midiEvents.forEach { midiEvent ->
-                            text.text = midiEvent.toString()
+        ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+            lifecycleScope.launch {
+                val packets = ReaStreamReceiver().packets
+                launch {
+                    AudioTrackOutput().play(packets)
+                }
+                launch {
+                    packets.filter { it.isMidi }
+                        .collect { packet ->
+                            packet.midiEvents.forEach { midiEvent ->
+                                text.text = midiEvent.toString()
+                            }
                         }
-                    }
+                }
             }
         }
     }
