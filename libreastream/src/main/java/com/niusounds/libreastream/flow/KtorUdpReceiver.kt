f9dea94dfd91bba1e6a4a30b4e7afe3be2e99d9b
Yuya Matsuo <niu@niusounds.com>
2021-08-21 18:50:37 +0900

    ktor as default Receiver

---
+++ libreastream/src/main/java/com/niusounds/libreastream/flow/KtorUdpReceiver.kt
@@ -0,0 +1,32 @@
+package com.niusounds.libreastream.flow
+
+import io.ktor.network.selector.*
+import io.ktor.network.sockets.*
+import io.ktor.utils.io.core.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.map
+import java.net.InetSocketAddress
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import kotlin.coroutines.CoroutineContext
+
+/**
+ * Receive UDP data with Ktor.
+ */
+class KtorUdpReceiver(
+    private val port: Int,
+    private val ioContext: CoroutineContext = Dispatchers.IO,
+) : PacketReceiver {
+    override fun receive(): Flow<ByteBuffer> {
+        val server = aSocket(ActorSelectorManager(ioContext))
+            .udp()
+            .bind(InetSocketAddress(port))
+
+        return server.incoming.consumeAsFlow()
+            .map {
+                it.packet.readByteBuffer().order(ByteOrder.LITTLE_ENDIAN)
+            }
+    }
+}
