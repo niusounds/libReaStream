ec5b29f8247e0b66465760bdf72a1db64228c0cf
Yuya Matsuo <niu@niusounds.com>
2021-08-29 20:41:31 +0900

    FloatArray.interleaved

--- libreastream/src/test/kotlin/com/niusounds/libreastream/sender/FloatArrayExtTest.kt
+++ libreastream/src/test/kotlin/com/niusounds/libreastream/sender/FloatArrayExtTest.kt
@@ -1,21 +1,49 @@
 package com.niusounds.libreastream.sender
 
-import com.niusounds.libreastream.receiver.ByteBufferReaStreamPacket
 import org.junit.Assert
 import org.junit.Test
 
 class FloatArrayExtTest {
     @Test
     fun testDeInterleave() {
+        // Interleaved data
         val data = floatArrayOf(
-            1f, 2f, 3f, 4f, 5f, 6f
+            // L R
+            1f, 2f,
+            3f, 4f,
+            5f, 6f,
         )
 
         val result = data.deInterleave(channels = 2)
 
         Assert.assertArrayEquals(
+            // Non-interleaved data
             floatArrayOf(
-                1f, 3f, 5f, 2f, 4f, 6f,
+                1f, 3f, 5f, // L
+                2f, 4f, 6f, // R
+            ),
+            result,
+            0.0001f,
+        )
+    }
+
+    @Test
+    fun testInterleave() {
+        // Non-interleaved data
+        val data = floatArrayOf(
+            1f, 2f, 3f, // L
+            4f, 5f, 6f, // R
+        )
+
+        val result = data.interleaved(channels = 2)
+
+        Assert.assertArrayEquals(
+            // Interleaved data
+            floatArrayOf(
+                // L R
+                1f, 4f,
+                2f, 5f,
+                3f, 6f,
             ),
             result,
             0.0001f,
