d920e6434ce2713329b4caeea7d4ae0c757c8dfb
Yuya Matsuo <niu@niusounds.com>
2021-10-17 02:16:57 +0900

    Record thread

--- libreastream/src/main/java/com/niusounds/libreastream/sender/AudioRecordInput.kt
+++ libreastream/src/main/java/com/niusounds/libreastream/sender/AudioRecordInput.kt
@@ -4,11 +4,14 @@ import android.annotation.SuppressLint
 import android.media.AudioFormat
 import android.media.AudioRecord
 import android.media.MediaRecorder
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.niusounds.libreastream.receiver.ReaStreamPacket
-import kotlinx.coroutines.currentCoroutineContext
+import kotlin.concurrent.thread
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.isActive
+import kotlinx.coroutines.flow.callbackFlow
 
 /**
  * To use this class, RECORD_AUDIO permission is required.
@@ -18,41 +21,55 @@ class AudioRecordInput(
     private val channels: Int,
     private val chunkSize: Int = ReaStreamPacket.MAX_BLOCK_LENGTH / Float.SIZE_BYTES,
 ) {
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @RequiresApi(Build.VERSION_CODES.M)
     @SuppressLint("MissingPermission")
-    fun readAudio(): Flow<FloatArray> = flow {
-        val audioSource = MediaRecorder.AudioSource.DEFAULT
-        val channelConfig = when (channels) {
-            1 -> AudioFormat.CHANNEL_IN_MONO
-            2 -> AudioFormat.CHANNEL_IN_STEREO
-            else -> error("unsupported channels")
-        }
-        val audioFormat = AudioFormat.ENCODING_PCM_FLOAT
-        val bufferSizeInBytes = AudioRecord.getMinBufferSize(sampleRate, channelConfig, audioFormat)
+    fun readAudio(): Flow<FloatArray> = callbackFlow {
+        val audioThread = thread {
+            android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO)
 
-        val record = AudioRecord.Builder()
-            .setAudioSource(audioSource)
-            .setAudioFormat(
-                AudioFormat.Builder()
-                    .setEncoding(audioFormat)
-                    .setSampleRate(sampleRate)
-                    .setChannelMask(channelConfig)
-                    .build()
-            )
-            .setBufferSizeInBytes(bufferSizeInBytes)
-            .build()
-
-        record.startRecording()
-
-        try {
-            val audioData = FloatArray(chunkSize)
-            while (currentCoroutineContext().isActive) {
-                record.read(audioData, 0, chunkSize, AudioRecord.READ_BLOCKING)
-                emit(audioData)
+            val audioSource = MediaRecorder.AudioSource.DEFAULT
+            val channelConfig = when (channels) {
+                1 -> AudioFormat.CHANNEL_IN_MONO
+                2 -> AudioFormat.CHANNEL_IN_STEREO
+                else -> error("unsupported channels")
             }
-        } finally {
-            record.stop()
-            record.release()
+            val audioFormat = AudioFormat.ENCODING_PCM_FLOAT
+            val bufferSizeInBytes = AudioRecord.getMinBufferSize(
+                sampleRate,
+                channelConfig,
+                audioFormat,
+            )
+
+            val record = AudioRecord.Builder()
+                .setAudioSource(audioSource)
+                .setAudioFormat(
+                    AudioFormat.Builder()
+                        .setEncoding(audioFormat)
+                        .setSampleRate(sampleRate)
+                        .setChannelMask(channelConfig)
+                        .build()
+                )
+                .setBufferSizeInBytes(bufferSizeInBytes)
+                .build()
+
+            record.startRecording()
+
+            try {
+                while (!Thread.interrupted()) {
+                    val audioData = FloatArray(chunkSize)
+                    record.read(audioData, 0, chunkSize, AudioRecord.READ_BLOCKING)
+                    if (trySend(audioData).isClosed) {
+                        break
+                    }
+                }
+            } finally {
+                record.stop()
+                record.release()
+            }
         }
+
+        awaitClose { audioThread.interrupt() }
     }
 }
 
