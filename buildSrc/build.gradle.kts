348dc858ac15fae9a1d583053dd41a6dc49f9362
Yuya Matsuo <niu@niusounds.com>
2022-10-01 15:32:54 +0900

    kotlin 1.7.10

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,5 +9,5 @@ repositories {
 
 dependencies {
     implementation("com.android.tools.build:gradle:7.3.0")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
 }
