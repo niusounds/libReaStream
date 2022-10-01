5610f981de67fc22228396102c4325d82118d13b
Yuya Matsuo <niu@niusounds.com>
2022-10-01 15:33:01 +0900

    sdk 33

--- buildSrc/src/main/kotlin/sample.conventions.gradle.kts
+++ buildSrc/src/main/kotlin/sample.conventions.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    compileSdk = 32
+    compileSdk = 33
 
     defaultConfig {
         minSdk = 24
-        targetSdk = 32
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
